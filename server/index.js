d726b1d376e2afa4c2e987c1c09dd3b4e4e5e4da
Ayush152 <aayushgarg152@gmail.com>
2024-01-09 13:00:19 +0530

    added express server code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(cors());
+// app.use(cors());
 app.use(router);
 
 io.on('connect', (socket) => {
@@ -62,4 +62,5 @@ io.on('connect', (socket) => {
 	});
 });
 
-server.listen(process.env.PORT, () => console.log(`Server has started.`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
